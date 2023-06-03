ef5e9dcc789ce7d07c0f205206b40d79c1eb3144
蔡略 <cailue@bupt.edu.cn>
2023-06-03 14:58:39 +0800

    refactor: change colour scheme
    Signed-off-by: 蔡略 <cailue@bupt.edu.cn>

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -45,7 +45,7 @@ settings["palette_overwrite"] = {}
 
 -- Set the colorscheme to use here.
 -- Available values are: `catppuccin`, `catppuccin-latte`, `catppucin-mocha`, `catppuccin-frappe`, `catppuccin-macchiato`, `edge`, `nord`.
-settings["colorscheme"] = "duskfox"
+settings["colorscheme"] = "nightfox"
 -- settings["colorscheme"] = "catppuccin"
 
 -- Set it to true if your terminal has transparent background.
